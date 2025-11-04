d0658819f4dfbe39c3151c659f37b46204f03532
Eze <ezequielc.mail@gmail.com>
2025-11-03 21:57:58 -0300

    Agrego testing de reportes 5 y 6

--- Entrega_6/Testing_Reportes.sql
+++ Entrega_6/Testing_Reportes.sql
@@ -6,12 +6,21 @@ GO
 --reporte 2
 
 -- Reporte 3
-EXEC  gestion.sp_reporte_recaudacion_por_procedencia  @anioInicio = 2025, @anioFin = 2025, @idConsorcio = null;
+EXEC gestion.sp_reporte_recaudacion_por_procedencia  @anioInicio = 2025, @anioFin = 2025, @idConsorcio = null;
 
 -- Reporte 4 en XML
 EXEC gestion.sp_reporte_mayores_ingresos_gastos_xml @id_consorcio = null, @anio_inicio = 2025, @anio_fin = 2025;
 
 --reporte 5
+EXEC gestion.sp_reporte_top_morosos;						-- default, top 3
+EXEC gestion.sp_reporte_top_morosos 2;						-- consorcio 2
+EXEC gestion.sp_reporte_top_morosos null, 100, 0;			-- top 100
+EXEC gestion.sp_reporte_top_morosos null, 100, 300000;		-- top 100, minimo 30000 de deuda
+EXEC gestion.sp_reporte_top_morosos null, 100, 0, 50000;	-- top 100, maximo 50000 de deuda
 
 --reporte 6
+EXEC gestion.sp_reporte_pagos_ordinarios;                                   -- default
+EXEC gestion.sp_reporte_pagos_ordinarios 2;                                 -- solo el consorcio 2
+EXEC gestion.sp_reporte_pagos_ordinarios null, '2024-10-01', '2024-10-31';  -- test con fechas
+
 
