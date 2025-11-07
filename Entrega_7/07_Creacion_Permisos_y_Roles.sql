5391b39ec94a047032a45784be884b8dbce99e43
sbfanton <sfanton.develop@gmail.com>
2025-11-06 22:11:51 -0300

    Se agregan sentencias de eliminacion de permisos, roles y usuarios

--- Entrega_7/07_Creacion_Permisos_y_Roles.sql
+++ Entrega_7/07_Creacion_Permisos_y_Roles.sql
@@ -1,22 +1,168 @@
-USE master
+/*=============================================================
+    LIMPIEZA Y CREACIÓN DE LOGINS, USUARIOS, ROLES Y PERMISOS
+=============================================================*/
+
+USE master;
 GO
 
+------------------------------------------------------------
+-- 1️⃣ ELIMINAR ROLES DE SERVIDOR Y MIEMBROS (SI EXISTEN)
+------------------------------------------------------------
+DECLARE @serverRoles TABLE (name SYSNAME)
+INSERT INTO @serverRoles VALUES
+('rol_administrativo_general'),
+('rol_administrativo_bancario'),
+('rol_administrativo_operativo'),
+('rol_sistemas')
+
+-- Quitar miembros de roles existentes
+DECLARE @dropMembers NVARCHAR(MAX) = (
+    SELECT STRING_AGG(
+        'ALTER SERVER ROLE [' + sp1.name + '] DROP MEMBER [' + sp2.name + '];',
+        CHAR(10)
+    )
+    FROM sys.server_role_members rm
+    JOIN sys.server_principals sp1 ON rm.role_principal_id = sp1.principal_id
+    JOIN sys.server_principals sp2 ON rm.member_principal_id = sp2.principal_id
+    WHERE sp1.name IN (SELECT name FROM @serverRoles)
+)
+
+IF @dropMembers IS NOT NULL
+    EXEC sys.sp_executesql @dropMembers
+
+-- Ahora eliminar los roles
+DECLARE @dropRoles NVARCHAR(MAX) = (
+    SELECT STRING_AGG(
+        'DROP SERVER ROLE [' + name + '];',
+        CHAR(10)
+    )
+    FROM @serverRoles
+    WHERE name IN (SELECT name FROM sys.server_principals)
+)
+
+IF @dropRoles IS NOT NULL
+    EXEC sys.sp_executesql @dropRoles
+GO
+
+
+------------------------------------------------------------
+-- 2️⃣ ELIMINAR LOGINS SI EXISTEN
+------------------------------------------------------------
+DECLARE @logins TABLE (name SYSNAME)
+INSERT INTO @logins VALUES
+('admin_general'),
+('admin_bancario'),
+('admin_operativo'),
+('sistemas')
+
+DECLARE @dropLogins NVARCHAR(MAX) = (
+    SELECT STRING_AGG('
+        IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = ''' + name + ''')
+            DROP LOGIN [' + name + '];', CHAR(10))
+    FROM @logins
+)
+
+EXEC sys.sp_executesql @dropLogins
+GO
+
+------------------------------------------------------------
+-- 3️⃣ CREAR LOGINS NUEVOS
+------------------------------------------------------------
 CREATE LOGIN admin_general WITH PASSWORD = 'AdmGen123!';
 CREATE LOGIN admin_bancario WITH PASSWORD = 'AdmBan123!';
 CREATE LOGIN admin_operativo WITH PASSWORD = 'AdmOpe123!';
 CREATE LOGIN sistemas WITH PASSWORD = 'Sist123!';
 GO
-  
 
+
+------------------------------------------------------------
+-- 4️⃣ CAMBIO DE BASE DE DATOS
+------------------------------------------------------------
 USE Com5600G08;
 GO
 
-CREATE USER admin_general FOR LOGIN admin_general WITH DEFAULT_SCHEMA= [gestion];
-CREATE USER admin_bancario FOR LOGIN admin_bancario WITH DEFAULT_SCHEMA= [gestion];
-CREATE USER admin_operativo FOR LOGIN admin_operativo WITH DEFAULT_SCHEMA= [gestion];
-CREATE USER sistemas FOR LOGIN sistemas WITH DEFAULT_SCHEMA= [gestion];
+------------------------------------------------------------
+-- 5️⃣ ELIMINAR ROLES Y USUARIOS DE BASE DE DATOS
+------------------------------------------------------------
+
+-- Roles de base de datos a limpiar
+DECLARE @roles TABLE (name SYSNAME)
+INSERT INTO @roles VALUES
+('rol_administrativo_general'),
+('rol_administrativo_bancario'),
+('rol_administrativo_operativo'),
+('rol_sistemas')
+
+------------------------------------------------------------
+-- 🔸 1. Quitar miembros de roles de base de datos
+------------------------------------------------------------
+DECLARE @dropMembers NVARCHAR(MAX)
+
+SELECT @dropMembers = STRING_AGG(
+    'ALTER ROLE [' + dp1.name + '] DROP MEMBER [' + dp2.name + '];',
+    CHAR(10)
+)
+FROM sys.database_role_members drm
+JOIN sys.database_principals dp1 ON drm.role_principal_id = dp1.principal_id
+JOIN sys.database_principals dp2 ON drm.member_principal_id = dp2.principal_id
+WHERE dp1.name IN (SELECT name FROM @roles)
+
+IF @dropMembers IS NOT NULL
+    EXEC sys.sp_executesql @dropMembers
+
+
+------------------------------------------------------------
+-- 🔸 2. Eliminar roles
+------------------------------------------------------------
+DECLARE @dropRoles NVARCHAR(MAX)
+
+SELECT @dropRoles = STRING_AGG(
+    'DROP ROLE [' + name + '];',
+    CHAR(10)
+)
+FROM @roles
+WHERE name IN (SELECT name FROM sys.database_principals)
+
+IF @dropRoles IS NOT NULL
+    EXEC sys.sp_executesql @dropRoles
+
+
+------------------------------------------------------------
+-- 🔸 3. Eliminar usuarios de la base
+------------------------------------------------------------
+DECLARE @users TABLE (name SYSNAME)
+INSERT INTO @users VALUES
+('admin_general'),
+('admin_bancario'),
+('admin_operativo'),
+('sistemas')
+
+DECLARE @dropUsers NVARCHAR(MAX)
+
+SELECT @dropUsers = STRING_AGG(
+    'DROP USER [' + name + '];',
+    CHAR(10)
+)
+FROM @users
+WHERE name IN (SELECT name FROM sys.database_principals)
+
+IF @dropUsers IS NOT NULL
+    EXEC sys.sp_executesql @dropUsers
 GO
 
+------------------------------------------------------------
+-- 6️⃣ CREAR NUEVOS USUARIOS
+------------------------------------------------------------
+CREATE USER admin_general FOR LOGIN admin_general WITH DEFAULT_SCHEMA = [gestion];
+CREATE USER admin_bancario FOR LOGIN admin_bancario WITH DEFAULT_SCHEMA = [gestion];
+CREATE USER admin_operativo FOR LOGIN admin_operativo WITH DEFAULT_SCHEMA = [gestion];
+CREATE USER sistemas FOR LOGIN sistemas WITH DEFAULT_SCHEMA = [gestion];
+GO
+
+------------------------------------------------------------
+-- 7️⃣ CREAR ROLES Y ASIGNAR USUARIOS
+------------------------------------------------------------
+-- Roles de servidor
 CREATE SERVER ROLE rol_administrativo_general;
 CREATE SERVER ROLE rol_administrativo_bancario;
 CREATE SERVER ROLE rol_administrativo_operativo;
@@ -24,11 +170,12 @@ CREATE SERVER ROLE rol_sistemas;
 
 ALTER SERVER ROLE rol_administrativo_general ADD MEMBER admin_general;
 ALTER SERVER ROLE rol_administrativo_bancario ADD MEMBER admin_bancario;
-ALTER SERVER ROLE bulkadmin ADD MEMBER admin_bancario;
+--ALTER SERVER ROLE bulkadmin ADD MEMBER admin_bancario;
 ALTER SERVER ROLE rol_administrativo_operativo ADD MEMBER admin_operativo;
 ALTER SERVER ROLE rol_sistemas ADD MEMBER sistemas;
+GO
 
-
+-- Roles de base de datos
 CREATE ROLE rol_administrativo_general;
 CREATE ROLE rol_administrativo_bancario;
 CREATE ROLE rol_administrativo_operativo;
@@ -40,78 +187,34 @@ ALTER ROLE rol_administrativo_operativo ADD MEMBER admin_operativo;
 ALTER ROLE rol_sistemas ADD MEMBER sistemas;
 GO
 
---PERMISOS PARA ADMINISTRATIVO GENERAL
+------------------------------------------------------------
+-- 8️⃣ ASIGNAR PERMISOS
+------------------------------------------------------------
+
+-- ADMINISTRATIVO GENERAL
 GRANT EXECUTE ON gestion.sp_modificar_Unidad_Funcional TO rol_administrativo_general;
 GRANT EXECUTE ON gestion.sp_reporte_recaudacion_por_procedencia TO rol_administrativo_general;
 GRANT EXECUTE ON gestion.sp_reporte_mayores_ingresos_gastos_xml TO rol_administrativo_general;
 GRANT EXECUTE ON gestion.ReportePagosOrdinarios TO rol_administrativo_general;
 GRANT EXECUTE ON gestion.TopMorosos TO rol_administrativo_general;
---[FALTA AGREGAR REPORTE 1 Y 2],[CAMBIARLE NOMBRE A LOS REPORTES, QUE EMPIECEN CON sp_reporte_(que hace)]
 
---PERMISOS PARA ADMINISTRATIVO BANCARIO
+-- ADMINISTRATIVO BANCARIO
 GRANT EXECUTE ON gestion.sp_importar_pagos TO rol_administrativo_bancario;
---alguno mas se considera info bancaria?
 GRANT EXECUTE ON gestion.sp_reporte_recaudacion_por_procedencia TO rol_administrativo_bancario;
 GRANT EXECUTE ON gestion.sp_reporte_mayores_ingresos_gastos_xml TO rol_administrativo_bancario;
 GRANT EXECUTE ON gestion.ReportePagosOrdinarios TO rol_administrativo_bancario;
 GRANT EXECUTE ON gestion.TopMorosos TO rol_administrativo_bancario;
---[FALTA AGREGAR REPORTE 1 Y 2],[CAMBIARLE NOMBRE A LOS REPORTES, QUE EMPIECEN CON sp_reporte_(que hace)]
 
---PERMISOS PARA ADMINISTRATIVO OPERATIVO
+-- ADMINISTRATIVO OPERATIVO
 GRANT EXECUTE ON gestion.sp_modificar_Unidad_Funcional TO rol_administrativo_operativo;
 GRANT EXECUTE ON gestion.sp_reporte_recaudacion_por_procedencia TO rol_administrativo_operativo;
 GRANT EXECUTE ON gestion.sp_reporte_mayores_ingresos_gastos_xml TO rol_administrativo_operativo;
 GRANT EXECUTE ON gestion.ReportePagosOrdinarios TO rol_administrativo_operativo;
 GRANT EXECUTE ON gestion.TopMorosos TO rol_administrativo_operativo;
---[FALTA AGREGAR REPORTE 1 Y 2],[CAMBIARLE NOMBRE A LOS REPORTES, QUE EMPIECEN CON sp_reporte_(que hace)]
 
---PERMISOS PARA SISTEMAS
+-- SISTEMAS
 GRANT EXECUTE ON gestion.sp_reporte_recaudacion_por_procedencia TO rol_sistemas;
 GRANT EXECUTE ON gestion.sp_reporte_mayores_ingresos_gastos_xml TO rol_sistemas;
 GRANT EXECUTE ON gestion.ReportePagosOrdinarios TO rol_sistemas;
 GRANT EXECUTE ON gestion.TopMorosos TO rol_sistemas;
---[FALTA AGREGAR REPORTE 1 Y 2],[CAMBIARLE NOMBRE A LOS REPORTES, QUE EMPIECEN CON sp_reporte_(que hace)]
-
-
---------------------
-	/*DETALLES*/
---------------------
--- Ver roles fijos de servidor y usuarios asignados
-SELECT SRM.role_principal_id, SP.name AS Role_Name,   
-SRM.member_principal_id, SP2.name  AS Member_Name  
-FROM sys.server_role_members AS SRM  
-JOIN sys.server_principals AS SP  
-    ON SRM.Role_principal_id = SP.principal_id  
-JOIN sys.server_principals AS SP2   
-    ON SRM.member_principal_id = SP2.principal_id  
-ORDER BY  SP.name,  SP2.name
---------------------
--- ver roles de la DB y usuarios asignados
-SELECT    roles.principal_id                            AS RolePrincipalID
-    ,    roles.name                                    AS RolePrincipalName
-    ,    database_role_members.member_principal_id    AS MemberPrincipalID
-    ,    members.name                                AS MemberPrincipalName
-FROM sys.database_role_members AS database_role_members  
-JOIN sys.database_principals AS roles  
-    ON database_role_members.role_principal_id = roles.principal_id  
-JOIN sys.database_principals AS members  
-    ON database_role_members.member_principal_id = members.principal_id;
---------------------
--- permisos otorgados explicitamente
-SELECT
-    perms.state_desc AS State,
-    permission_name AS [Permission],
-    obj.name AS [on Object],
-    dp.name AS [to User Name]
-FROM sys.database_permissions AS perms
-JOIN sys.database_principals AS dp
-    ON perms.grantee_principal_id = dp.principal_id
-JOIN sys.objects AS obj
-    ON perms.major_id = obj.object_id;
-
---------------------------------------------
-
-DROP LOGIN admin_general
-DROP LOGIN admin_bancario
-DROP LOGIN admin_operativo
-DROP LOGIN sistemas
+GO
